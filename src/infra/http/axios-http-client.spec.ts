cb8cd55116ccb0ea4bf16272178fc953bdf259ec
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-18 19:04:41 -0300

    refactor: ensure it returns correct response

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -1,5 +1,5 @@
-import { HttpClientRequest } from '../../data/protocols/http/http-client'
-import axios from 'axios'
+import { HttpClient, HttpClientRequest, HttpClientResponse } from '../../data/protocols/http/http-client'
+import axios, { AxiosResponse } from 'axios'
 
 jest.mock('axios')
 
@@ -28,18 +28,51 @@ const mockAxios = (): jest.Mocked<typeof axios> => {
   return mockedAxios
 }
 
+class AxiosHttpClient implements HttpClient {
+  async request (data: HttpClientRequest): Promise<HttpClientResponse> {
+    let axiosReponse: AxiosResponse
+    try {
+      axiosReponse = await axios.request({
+        url: data.url,
+        method: data.method,
+        data: data.body,
+        headers: data.headers
+      })
+    } catch (error) {
+      axiosReponse = error.response
+    }
+    return {
+      statusCode: axiosReponse.status,
+      body: axiosReponse.data
+    }
+  }
+}
+
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct values', async () => {
     const requestData = MockHttpRequest()
-    const sut = mockAxios()
+    const sut = new AxiosHttpClient()
+    const mockedAxios = mockAxios()
 
     await sut.request(requestData)
 
-    expect(sut.request).toHaveBeenCalledWith({
+    expect(mockedAxios.request).toHaveBeenCalledWith({
       url: requestData.url,
-      body: requestData.body,
+      data: requestData.body,
       headers: requestData.headers,
       method: requestData.method
+    })
+  })
+  test('Should return correct response', async () => {
+    const sut = new AxiosHttpClient()
+    const mockedAxios = mockAxios()
+
+    const httpResponse = await sut.request(MockHttpRequest())
+    const axiosResponse = await mockedAxios.request.mock.results[0].value
+
+    expect(httpResponse).toEqual({
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
     })
   })
 })
