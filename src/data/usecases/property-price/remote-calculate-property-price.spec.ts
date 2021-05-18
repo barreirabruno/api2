53bd75d2d71ac6c327978d651485ab16b77d673b
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-18 18:44:39 -0300

    test: ensure it throws ServerError if HttpClient returns 500

--- src/data/usecases/property-price/remote-calculate-property-price.spec.ts
+++ src/data/usecases/property-price/remote-calculate-property-price.spec.ts
@@ -1,13 +1,15 @@
 import { RemoteCalculatePropertyPrice } from './remote-calculate-property-price'
-import { mockPropertyPrice } from '../../../domain/models/mocks/property-price'
 import { HttpClientSpy } from '../../protocols/http/mocks/http-client'
+import { HttpStatusCode } from '../../protocols/http/http-client'
+import { UnexpectedError } from '../../../presentation/errors/unexpected-error'
+import { mockMeterPrice } from '../../../domain/models/mocks/meter-price'
 
 interface SutTypes {
   httpClientSpy: HttpClientSpy
   sut: RemoteCalculatePropertyPrice
 }
 
-const makeSut = (url: string): SutTypes => {
+const makeSut = (url: string = 'https://any_url'): SutTypes => {
   const httpClientSpy = new HttpClientSpy()
   const sut = new RemoteCalculatePropertyPrice(url, httpClientSpy)
   return {
@@ -18,17 +20,30 @@ const makeSut = (url: string): SutTypes => {
 
 describe('Calculate property price', () => {
   test('Should call HttpClient to get meter price', async () => {
-    const fakeUrl = 'https://any_url'
-    const { sut, httpClientSpy } = makeSut(fakeUrl)
+    const { sut, httpClientSpy } = makeSut()
     await sut.calculate()
 
-    expect(httpClientSpy.url).toBe(fakeUrl)
+    expect(httpClientSpy.url).toBe('https://any_url')
     expect(httpClientSpy.method).toBe('get')
   })
+  test('Should throw ServerError if HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+
+    const promise = sut.calculate()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
   test('Should return a property price on success', async () => {
-    const fakeUrl = 'https://any_url'
-    const { sut } = makeSut(fakeUrl)
+    const httpResult = mockMeterPrice()
+    const { sut, httpClientSpy } = makeSut('https://any_url')
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
     const remoteCalculatePropertyPriceResponse = await sut.calculate()
-    expect(remoteCalculatePropertyPriceResponse).toEqual(mockPropertyPrice(200))
+    expect(remoteCalculatePropertyPriceResponse).toEqual(httpResult)
   })
 })
