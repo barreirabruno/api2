a899d4b3660a0d99e0d10eb8825f195d76705db3
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-18 18:43:12 -0300

    feat: add meter price model and mock

---
+++ src/domain/models/meter-price.ts
@@ -0,0 +1,5 @@
+export interface MeterPriceModel {
+  id: string
+  district: string
+  price: string
+}
---
+++ src/domain/models/mocks/meter-price.ts
@@ -0,0 +1,9 @@
+import { MeterPriceModel } from '../meter-price'
+
+export const mockMeterPrice = (): MeterPriceModel => {
+  return {
+    id: 'any_id',
+    district: 'any_location',
+    price: '9650.80'
+  }
+}
