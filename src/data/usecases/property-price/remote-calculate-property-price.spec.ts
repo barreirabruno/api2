752e59c74c91c5984eeac0c400d55e15df6797f1
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-18 13:23:23 -0300

    test: should return a property price on success

---
+++ src/data/usecases/property-price/remote-calculate-property-price.spec.ts
@@ -0,0 +1,48 @@
+import { PropertyPriceModel } from '../../../domain/models/property-price'
+import { PropertyPrice } from '../../../domain/usecases/calculate-property-price'
+import { mockPropertyPrice } from '../../../domain/models/mocks/property-price'
+import { HttpClient, HttpClientRequest, HttpClientResponse, HttpStatusCode } from '../../protocols/http/http-client'
+
+class HttpClientSpy implements HttpClient {
+  url?: string
+  method?: string
+  body?: any
+  headers?: any
+  response: HttpClientResponse = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async request (data: HttpClientRequest): Promise<HttpClientResponse> {
+    this.url = data.url
+    this.method = data.method
+    this.body = data.body
+    this.headers = data.headers
+    return this.response
+  }
+}
+
+class CalculatePropertyPrice implements PropertyPrice {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient
+  ) { }
+
+  async calculate (): Promise<PropertyPriceModel> {
+    await this.httpClient.request({
+      url: this.url,
+      method: 'get'
+    })
+
+    return mockPropertyPrice(200)
+  }
+}
+
+describe('Calculate property price', () => {
+  test('Should return a property price on success', async () => {
+    const url = 'https://any_url'
+    const httpClientSpy = new HttpClientSpy()
+    const propertyPrice = new CalculatePropertyPrice(url, httpClientSpy)
+    const sut = await propertyPrice.calculate()
+    expect(sut).toEqual(mockPropertyPrice(200))
+  })
+})
