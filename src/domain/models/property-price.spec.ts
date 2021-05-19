427f0ef8319bded2f9decdd310335698d300d059
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-18 23:58:42 -0300

    test: ensure it can calculate the property price

--- src/domain/models/property-price.spec.ts
+++ src/domain/models/property-price.spec.ts
@@ -1,13 +1,8 @@
-export class CalculatePropertyPrice {
-  propertyPrice (landSize: number, meterPrice: number): number {
-    return landSize * meterPrice
-  }
-}
+import { calculatePropertyPrice } from './property-price'
 
 describe('Calculate property price', () => {
   test('Should calculate the property price', () => {
-    const sut = new CalculatePropertyPrice()
-    const propertyPrice = sut.propertyPrice(10, 10)
-    expect(propertyPrice).toBe(100)
+    const sut = calculatePropertyPrice(10, 10)
+    expect(sut).toBe(100)
   })
 })
