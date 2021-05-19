35d6bd0451da80c578b6839fa573d1579a043059
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-19 00:01:14 -0300

    refactor: call calculatePropertyPrice method when httpClient response succeeds

--- src/data/usecases/property-price/remote-calculate-property-price.ts
+++ src/data/usecases/property-price/remote-calculate-property-price.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/space-before-function-paren */
 /* eslint-disable space-before-function-paren */
-import { PropertyPriceModel } from '../../../domain/models/property-price'
+import { PropertyPriceModel, calculatePropertyPrice } from '../../../domain/models/property-price'
 import { PropertyPrice } from '../../../domain/usecases/calculate-property-price'
 import { UnexpectedError } from '../../../presentation/errors/unexpected-error'
 import { HttpClient, HttpStatusCode } from '../../protocols/http/http-client'
@@ -16,8 +16,16 @@ export class RemoteCalculatePropertyPrice implements PropertyPrice {
       url: this.url,
       method: 'get'
     })
+
     switch (httpClientResponse.statusCode) {
-      case HttpStatusCode.ok: return httpClientResponse.body
+      case HttpStatusCode.ok: {
+        const propertyPrice = calculatePropertyPrice(landSize, Number(httpClientResponse.body.price))
+        return {
+          landSize,
+          squareMeterPrice: httpClientResponse.body.price,
+          propertyPrice
+        }
+      }
       case HttpStatusCode.serverError: throw new UnexpectedError()
     }
   }
