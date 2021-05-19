e0ed52c96f62b44829c51e9dc5e86404fbc9d892
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-19 00:36:28 -0300

    refactor: use express adapter on routes

--- src/main/routes/calculate-property-price-routes.ts
+++ src/main/routes/calculate-property-price-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeCalculatePropertyPriceController } from '../factories/calculate-property-price'
 
 export default (router: Router): void => {
-  router.post('/calculate-property-price', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/calculate-property-price', adaptRoute(makeCalculatePropertyPriceController()))
 }
