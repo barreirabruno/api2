0f17d496719e9747cde5072d216fbac8cb7249e9
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-18 21:29:54 -0300

    refactor: fix route on integration test

--- src/main/routes/calculate-property-price-routes.test.ts
+++ src/main/routes/calculate-property-price-routes.test.ts
@@ -4,7 +4,7 @@ import app from '../config/app'
 describe('Calculate property price routes', () => {
   test('Should return a property price on success', async () => {
     await request(app)
-      .post('/api//calculate-property-price')
+      .post('/api/calculate-property-price')
       .send({ landSize: 1000 })
       .expect(200)
   })
